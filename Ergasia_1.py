e7f8a8bb0c01e858250c086b6d8448e7d40cae7e
R4GN4R <jimchr2001@gmail.com>
2020-01-10 19:32:51 +0200

    Update Ergasia_1.py

--- Ergasia_1.py
+++ Ergasia_1.py
@@ -1,6 +1,4 @@
-import re
-words = text.readlines()
-print(words)
+words = "Hello I like lookies cookies1"
 words = words.split()
 sortedwords = sorted(words, key=len)
 print("The longest word in the list is: %s." % (sortedwords[-1],))
@@ -8,4 +6,3 @@ print("The longest word in the list is: %s." % (sortedwords[-2],))
 print("The longest word in the list is: %s." % (sortedwords[-3],))
 print("The longest word in the list is: %s." % (sortedwords[-4],))
 print("The longest word in the list is: %s." % (sortedwords[-5],))
-# https://stackoverflow.com/questions/26132770/python-finding-longest-shortest-words-in-a-list-and-calling-them-in-a-function/44919337
