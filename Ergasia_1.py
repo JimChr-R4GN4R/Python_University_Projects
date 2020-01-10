54a716c05fee3f47053aadd8d5e043c6e5391c37
R4GN4R <jimchr2001@gmail.com>
2020-01-10 19:29:36 +0200

    Create Ergasia_1.py

---
+++ Ergasia_1.py
@@ -0,0 +1,10 @@
+import re
+words = text.readlines()
+print(words)
+words = words.split()
+sortedwords = sorted(words, key=len)
+print("The longest word in the list is: %s." % (sortedwords[-1],))
+print("The longest word in the list is: %s." % (sortedwords[-2],))
+print("The longest word in the list is: %s." % (sortedwords[-3],))
+print("The longest word in the list is: %s." % (sortedwords[-4],))
+print("The longest word in the list is: %s." % (sortedwords[-5],))
