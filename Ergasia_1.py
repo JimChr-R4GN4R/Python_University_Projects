b899e54a1a051f993d965fc5d9185902b6b89a38
R4GN4R <jimchr2001@gmail.com>
2020-01-10 19:33:07 +0200

    Update Ergasia_1.py

--- Ergasia_1.py
+++ Ergasia_1.py
@@ -6,3 +6,4 @@ print("The longest word in the list is: %s." % (sortedwords[-2],))
 print("The longest word in the list is: %s." % (sortedwords[-3],))
 print("The longest word in the list is: %s." % (sortedwords[-4],))
 print("The longest word in the list is: %s." % (sortedwords[-5],))
+# https://stackoverflow.com/questions/26132770/python-finding-longest-shortest-words-in-a-list-and-calling-them-in-a-function/44919337
