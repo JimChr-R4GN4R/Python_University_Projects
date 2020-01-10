66dd5b054ae5659221824ee01364efba8a511614
R4GN4R <jimchr2001@gmail.com>
2020-01-10 21:59:49 +0200

    Update Askhsh_1.py

--- Askhsh_1.py
+++ Askhsh_1.py
@@ -4,44 +4,19 @@ words = words.split() # converts $words variable from string to list of words ##
 sorted_list = sorted(words, key=len) # put's the words from the sortest to the longet
 reversed_list = sorted_list[::-1] # the list is reversed, so it goes from longest to sortest ## https://www.programiz.com/python-programming/methods/list/reverse
 
-
-################################## Vowels remover ##################################
-
-
-
-
-
 vowels = ('a', 'e', 'i', 'o', 'u', 'A', 'I', 'E', 'O', 'U')
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#########################################################################
 
 
 try:
-  long_word_0 = reversed_list[0][::-1]
-  for char in long_word_0:
-    if char in vowels:
-      long_word_0 = long_word_0.replace(char, '')
-  print("1st longest word in the list is: %s" % (long_word_0,))
+  long_word_0 = reversed_list[0][::-1] # reverse the 1st longest word
+  for char in long_word_0:                         ## 
+    if char in vowels:                             #### Removes every vowel the word has ## https://stackoverflow.com/questions/21581824/correct-code-to-remove-the-vowels-from-a-string-in-python
+      long_word_0 = long_word_0.replace(char, '')  ##
+  print("1st longest word in the list is: %s" % (long_word_0,)) # Prints the final result (reversed and with no vowels)
 
   try:
     long_word_1 = reversed_list[1][::-1]
@@ -95,5 +70,5 @@ except IndexError:
 ## https://stackoverflow.com/questions/26132770/python-finding-longest-shortest-words-in-a-list-and-calling-them-in-a-function/44919337
 ## https://discuss.codecademy.com/t/how-can-i-check-if-an-index-is-valid/377316/4
 ## https://stackoverflow.com/questions/17015230/are-nested-try-except-blocks-in-python-a-good-programming-practice
-
+##
 			
