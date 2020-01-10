1dde102dfd8b01faa6c7dcb0ef0e2ccf46cdf1a4
R4GN4R <jimchr2001@gmail.com>
2020-01-10 21:56:55 +0200

    Update Askhsh_1.py

--- Askhsh_1.py
+++ Askhsh_1.py
@@ -5,39 +5,86 @@ sorted_list = sorted(words, key=len) # put's the words from the sortest to the l
 reversed_list = sorted_list[::-1] # the list is reversed, so it goes from longest to sortest ## https://www.programiz.com/python-programming/methods/list/reverse
 
 
+################################## Vowels remover ##################################
+
+
+
+
+
+vowels = ('a', 'e', 'i', 'o', 'u', 'A', 'I', 'E', 'O', 'U')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 try:
+  long_word_0 = reversed_list[0][::-1]
+  for char in long_word_0:
+    if char in vowels:
+      long_word_0 = long_word_0.replace(char, '')
+  print("1st longest word in the list is: %s" % (long_word_0,))
 
-   print("1st longest word in the list is: %s" % (reversed_list[0],))
+  try:
+    long_word_1 = reversed_list[1][::-1]
+    for char in long_word_1:
+      if char in vowels:
+        long_word_1 = long_word_1.replace(char, '')
+    print("2nd longest word in the list is: %s" % (long_word_1,))
 
-   try:
+    try:
+      long_word_2 = reversed_list[2][::-1]
+      for char in long_word_2:
+        if char in vowels:
+          long_word_2 = long_word_2.replace(char, '')
+      print("2nd longest word in the list is: %s" % (long_word_2,))
 
-     print("2nd longest word in the list is: %s" % (reversed_list[1],))
+      try:
+        long_word_3 = reversed_list[3][::-1]
+        for char in long_word_3:
+          if char in vowels:
+            long_word_0 = long_word_3.replace(char, '')
+        print("4th longest word in the list is: %s" % (long_word_3,))
 
-     try:
-
-       print("3rd longest word in the list is: %s" % (reversed_list[2],))
-
-       try:
-
-         print("4th longest word in the list is: %s" % (reversed_list[3],))
-
-         try:
-           print("5th longest word in the list is: %s" % (reversed_list[4],))
+        try:
+          long_word_4 = reversed_list[4][::-1]
+          for char in long_word_4:
+            if char in vowels:
+              long_word_0 = long_word_4.replace(char, '')
+          print("5th longest word in the list is: %s" % (long_word_4,))
         
         
         
         
-         except IndexError:
-           print("There are no more words")
+        except IndexError:
+          print("There are no more words")
 
-       except IndexError:
-         print("There are no more words")
+      except IndexError:
+        print("There are no more words")
 
-     except IndexError:
-       print("There are no more words")
+    except IndexError:
+      print("There are no more words")
 
-   except IndexError:
-     print("There are no more words")
+  except IndexError:
+    print("There are no more words")
 
 except IndexError:
   print("Text file is empty! Please put some words :)")
@@ -48,3 +95,5 @@ except IndexError:
 ## https://stackoverflow.com/questions/26132770/python-finding-longest-shortest-words-in-a-list-and-calling-them-in-a-function/44919337
 ## https://discuss.codecademy.com/t/how-can-i-check-if-an-index-is-valid/377316/4
 ## https://stackoverflow.com/questions/17015230/are-nested-try-except-blocks-in-python-a-good-programming-practice
+
+			
