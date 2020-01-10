1d459d8559d17cf214e30740b68325ae75bbccbc
R4GN4R <jimchr2001@gmail.com>
2020-01-10 19:44:35 +0200

    Update Ergasia_1.py

--- Ergasia_1.py
+++ Ergasia_1.py
@@ -1,9 +1,10 @@
 words = "Hello I like lookies cookies1"
 words = words.split() # takes $words' input and puts it's words in a list 
-sortedwords = sorted(words, key=len) # puts the words from the shortest to the longest
-print("The longest word in the list is: %s." % (sortedwords[-1],)) # 1st longest word
-print("The longest word in the list is: %s." % (sortedwords[-2],)) # 2nd longest word
-print("The longest word in the list is: %s." % (sortedwords[-3],)) # 3rd longest word
-print("The longest word in the list is: %s." % (sortedwords[-4],)) # 4th longest word
-print("The longest word in the list is: %s." % (sortedwords[-5],)) # 5th longest word
+sorted_list = sorted(words, key=len) # put's the words from the sortest to the longet
+reversed_list = sorted_list[::-1] # the list is reversed, so it goes from longest to sortest
+print("The longest word in the list is: %s." % (sortedwords[0],)) # 1st longest word
+print("The longest word in the list is: %s." % (sortedwords[1],)) # 2nd longest word
+print("The longest word in the list is: %s." % (sortedwords[2],)) # 3rd longest word
+print("The longest word in the list is: %s." % (sortedwords[3],)) # 4th longest word
+print("The longest word in the list is: %s." % (sortedwords[4],)) # 5th longest word
 # https://stackoverflow.com/questions/26132770/python-finding-longest-shortest-words-in-a-list-and-calling-them-in-a-function/44919337
