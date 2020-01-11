b5125590062854df1b3bfae14cd81da62be3c11a
R4GN4R <jimchr2001@gmail.com>
2020-01-11 12:20:50 -0500

    Update Askhsh_2.py

--- Askhsh_2.py
+++ Askhsh_2.py
@@ -1,36 +1,45 @@
-#with open('text.txt', 'r') as file: ## https://stackoverflow.com/questions/8369219/how-to-read-a-text-file-into-a-string-variable-and-strip-newlines
-#words = file.read().replace('\n', ' ') # replace in text.txt the change line with space
-#words = words.split() # converts $words variable from string to list of words ## https://stackoverflow.com/questions/6181763/converting-a-string-to-a-list-of-words
 
-
-
-###########################################################################################
-
-# https://stackoverflow.com/questions/40985203/counting-letter-frequency-in-a-string-python
-string=input()
 f={}
 
+def freq(str): 
+  
+    # break the string into list of words  
+    str = str.split()          
+    str2 = [] 
+  
+    # loop till string values present in list str 
+    for i in str:              
+  
+        # checking for the duplicacy 
+        if i not in str2: 
+  
+            # insert value in str2 
+            str2.append(i)  
+              
+    for i in range(0, len(str2)): 
+  
+        # count the frequency of each word(present  
+        # in str2) in str and print 
+        word_letters_number = 0
+        for j in str2[i]:
+          f[i]=f.get(i,0)+1
+          word_letters_number = word_letters_number + 1
+          f_number = int(str2[i].count('f')) # number of f 
+          c_number = int(str2[i].count('c')) # number of c
+          k_number = int(str2[i].count('k')) # number of k
+          r_number = int(str2[i].count('r')) # number of r
+          total_bad_letters = f_number + c_number + k_number + r_number
+          total_good_letters = int(word_letters_number - total_bad_letters)
 
-word_letters_number = 0 # word's letters' number
-
-
-for i in string:
-  f[i]=f.get(i,0)+1
-  word_letters_number = word_letters_number + 1 
-
-
-
-# https://stackoverflow.com/questions/2932511/letter-count-on-a-string
-f_number = int(string.count('f')) # number of f 
-c_number = int(string.count('c')) # number of c
-k_number = int(string.count('k')) # number of k
-r_number = int(string.count('r')) # number of r
-
-total_bad_letters = f_number + c_number + k_number + r_number
-total_good_letters = int(word_letters_number - total_bad_letters)
-
-if total_good_letters > total_bad_letters:
-  print("Good word")
-else:
-  print("Bad word! Pepper on your tongue!")
-
+        if total_good_letters > total_bad_letters:
+          print(str2[i], 'is : Good word')  
+        else:
+          print(str2[i], 'is : Bad word! Pepper on your tongue!')   
+  
+def main(): 
+    with open('text.txt', 'r') as myfile:
+      str=myfile.read().replace('\n', ' ')
+    freq(str)                     
+  
+if __name__=="__main__": 
+    main()             # call main function 
