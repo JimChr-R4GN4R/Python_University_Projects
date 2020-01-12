7124ceff91e8cb8ecbdf99f05b125841e1dd2b40
R4GN4R <jimchr2001@gmail.com>
2020-01-12 08:36:28 -0500

    Update script.py

--- Askhsh_3/script.py
+++ Askhsh_3/script.py
@@ -6,7 +6,8 @@ prices = {
     "pear": 3
 }
 ##############################################
-
+cost = 0
+foros = 0
 
 def compute_bill(food):
     total = 0
@@ -20,15 +21,24 @@ def compute_bill(food):
 shopping_list = [] 
 while len(shopping_list) < 100: # maximum items you can add are 100
     item = input("Enter your Item to the List: ")
+
     if item == '': # If finished and press enter, then stop the loop
       break
-    shopping_list.append(item)
+
+    try: # Checks if input exists in the list
+      shopping_list.append(item)
+      cost = sum([ prices[s] for s in shopping_list ])
+    except KeyError:
+      print("Item does not exist in the list")
+      shopping_list = shopping_list[:-1] # if input is not exist,then delete it from $shopping_list
+
+
 #####################################################################
 
 
 
-cost = sum([ prices[s] for s in shopping_list ])
-foros = cost * 24 / 100
+
+
 
 sum_cost = foros + cost
 
@@ -39,3 +49,4 @@ print(sum_cost, "€")
 ###########################
 #https://stackoverflow.com/questions/29189978/computing-shopping-list-total-using-dictionaries
 #https://stackoverflow.com/questions/21043387/how-do-you-add-input-from-user-into-list-in-python#21043528
+#https://stackoverflow.com/questions/18169965/how-to-delete-last-item-in-list#18170372
