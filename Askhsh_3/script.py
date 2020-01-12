ed983bd142c74770e9783dda81bffb252111fa7d
R4GN4R <jimchr2001@gmail.com>
2020-01-12 08:20:07 -0500

    Update script.py

--- Askhsh_3/script.py
+++ Askhsh_3/script.py
@@ -1,11 +1,12 @@
+###### Available items with their costs ######
 prices = {
     "banana": 100,
     "apple": 2,
     "orange": 1.5,
     "pear": 3
 }
+##############################################
 
-# Write your code below!
 
 def compute_bill(food):
     total = 0
@@ -31,10 +32,10 @@ foros = cost * 24 / 100
 
 sum_cost = foros + cost
 
-print(sum_cost)
+print(sum_cost, "€")
 
 
 
-############################# Useful sources for the script #############################
+###########################
 #https://stackoverflow.com/questions/29189978/computing-shopping-list-total-using-dictionaries
 #https://stackoverflow.com/questions/21043387/how-do-you-add-input-from-user-into-list-in-python#21043528
