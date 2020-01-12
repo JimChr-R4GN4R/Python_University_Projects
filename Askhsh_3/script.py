f2530a30adadb610dc2d12daf17a80c58e191bc2
R4GN4R <jimchr2001@gmail.com>
2020-01-12 07:56:38 -0500

    Create script.py

---
+++ Askhsh_3/script.py
@@ -0,0 +1,28 @@
+prices = {
+    "banana": 100,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+# Write your code below!
+
+def compute_bill(food):
+    total = 0
+    for item in food:
+        item = shopping_list(prices[key])
+        total += item
+    return total
+shopping_list = ["banana", "orange", "apple"]  
+
+cost = sum([ prices[s] for s in shopping_list ])
+foros = cost * 24 / 100
+
+sum_cost = foros + cost
+
+print(sum_cost)
+
+
+
+###########################
+https://stackoverflow.com/questions/29189978/computing-shopping-list-total-using-dictionaries
