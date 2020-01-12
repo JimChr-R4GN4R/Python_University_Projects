0a2d954916d23c05867246f394a2896fde13a77b
R4GN4R <jimchr2001@gmail.com>
2020-01-12 08:12:52 -0500

    Update script.py

--- Askhsh_3/script.py
+++ Askhsh_3/script.py
@@ -13,7 +13,21 @@ def compute_bill(food):
         item = shopping_list(prices[key])
         total += item
     return total
-shopping_list = ["banana", "orange", "apple"]  
+
+###################
+
+shopping_list = [] 
+maxLengthList = 6
+while len(shopping_list) < maxLengthList:
+    item = input("Enter your Item to the List: ")
+    shopping_list.append(item)
+    print(shopping_list)
+print("That's your Shopping List")
+print(shopping_list)
+
+##################
+
+
 
 cost = sum([ prices[s] for s in shopping_list ])
 foros = cost * 24 / 100
@@ -25,4 +39,5 @@ print(sum_cost)
 
 
 ###########################
-https://stackoverflow.com/questions/29189978/computing-shopping-list-total-using-dictionaries
+#https://stackoverflow.com/questions/29189978/computing-shopping-list-total-using-dictionaries
+#https://stackoverflow.com/questions/21043387/how-do-you-add-input-from-user-into-list-in-python#21043528
