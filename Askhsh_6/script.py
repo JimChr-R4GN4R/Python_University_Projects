ea275a917c352b144a661b62411af9d8f5bcbda5
R4GN4R <jimchr2001@gmail.com>
2020-01-14 07:55:51 -0500

    Add files via upload

---
+++ Askhsh_6/script.py
@@ -0,0 +1,16 @@
+from scraper import start_script
+
+
+
+target = input("Choose your target: ")
+while target == "":
+    print("Please put a name!\n")
+    target = input("Choose your target: ")
+
+start_script(target)
+
+
+# https://community.esri.com/thread/231888-calling-a-python-script-from-another-python-script
+# https://stackoverflow.com/questions/48395685/launch-a-python-script-from-another-script-with-parameters-in-subprocess-argume
+# https://stackoverflow.com/questions/31483655/how-to-select-specific-json-element-in-python
+# https://stackoverflow.com/questions/8114355/loop-until-a-specific-user-input
