0d7344647074414f6a2a83f405b88cc4a0853a84
R4GN4R <jimchr2001@gmail.com>
2020-02-01 11:08:20 -0500

    Update script.py

--- Askhsh_2/script.py
+++ Askhsh_2/script.py
@@ -1,63 +1,65 @@
-with open('text.txt') as my_file:
-    # I already have file open at this point.. now what?
-    my_file.seek(0) #ensure you're at the start of the file..
-    first_char = my_file.read(1) #get the first character
-    if not first_char:
-        print("Text file is empty!") #first character is the empty string..
-    else:
-        my_file.seek(0) #first character wasn't empty, return to start of file.
+try:
+  with open('text.txt') as my_file:
+      # I already have file open at this point.. now what?
+      my_file.seek(0) #ensure you're at the start of the file..
+      first_char = my_file.read(1) #get the first character
+      if not first_char:
+          print("Text file is empty!") #first character is the empty string..
+      else:
+          my_file.seek(0) #first character wasn't empty, return to start of file.
 
 
-        f={}
+          f={}
 
-        def freq(str): 
-          
-            # break the string into list of words  
-            str = str.split()          
-            str2 = [] 
-          
-            # loop till string values present in list str 
-            for i in str:              
-          
-                # checking for the duplicacy 
-                if i not in str2: 
-          
-                    # insert value in str2 
-                    str2.append(i)  
-                      
-            for i in range(0, len(str2)): 
-          
-                # count the frequency of each word(present  
-                # in str2) in str and print 
-                word_letters_number = 0
-                for j in str2[i]:
-                  f[i]=f.get(i,0)+1
-                  word_letters_number = word_letters_number + 1
-                  f_number = int(str2[i].count('f')) # number of f 
-                  c_number = int(str2[i].count('c')) # number of c
-                  k_number = int(str2[i].count('k')) # number of k
-                  r_number = int(str2[i].count('r')) # number of r
-                  F_number = int(str2[i].count('F')) # number of F 
-                  C_number = int(str2[i].count('C')) # number of C
-                  K_number = int(str2[i].count('K')) # number of K
-                  R_number = int(str2[i].count('R')) # number of R
-                  total_bad_letters = f_number + c_number + k_number + r_number + F_number + C_number + K_number + R_number
-                  total_good_letters = int(word_letters_number - total_bad_letters)
+          def freq(str): 
+            
+              # break the string into list of words  
+              str = str.split()          
+              str2 = [] 
+            
+              # loop till string values present in list str 
+              for i in str:              
+            
+                  # checking for the duplicacy 
+                  if i not in str2: 
+            
+                      # insert value in str2 
+                      str2.append(i)  
+                        
+              for i in range(0, len(str2)): 
+            
+                  # count the frequency of each word(present  
+                  # in str2) in str and print 
+                  word_letters_number = 0
+                  for j in str2[i]:
+                    f[i]=f.get(i,0)+1
+                    word_letters_number = word_letters_number + 1
+                    f_number = int(str2[i].count('f')) # number of f 
+                    c_number = int(str2[i].count('c')) # number of c
+                    k_number = int(str2[i].count('k')) # number of k
+                    r_number = int(str2[i].count('r')) # number of r
+                    F_number = int(str2[i].count('F')) # number of F 
+                    C_number = int(str2[i].count('C')) # number of C
+                    K_number = int(str2[i].count('K')) # number of K
+                    R_number = int(str2[i].count('R')) # number of R
+                    total_bad_letters = f_number + c_number + k_number + r_number + F_number + C_number + K_number + R_number
+                    total_good_letters = int(word_letters_number - total_bad_letters)
 
-                if total_good_letters > total_bad_letters:
-                  print(str2[i], 'is : Good word')  
-                else:
-                  print(str2[i], 'is : Bad word! Pepper on your tongue!')   
-          
-        def main(): 
-            with open('text.txt', 'r') as myfile:
-              str=myfile.read().replace(',', '\n').replace('\n', ' ',).replace('.', '').replace('!', '').replace('-', '').replace('_', '').replace('(', '') .replace(')', '')  # replace \n and delete , ! - _ and dot
-            freq(str)                     
-          
-        if __name__=="__main__": 
-            main()             # call main function 
+                  if total_good_letters > total_bad_letters:
+                    print(str2[i], 'is : Good word')  
+                  else:
+                    print(str2[i], 'is : Bad word! Pepper on your tongue!')   
+            
+          def main(): 
+              with open('text.txt', 'r') as myfile:
+                str=myfile.read().replace(',', '\n').replace('\n', ' ',).replace('.', '').replace('!', '').replace('-', '').replace('_', '').replace('(', '') .replace(')', '')  # replace \n and delete , ! - _ and dot
+              freq(str)                     
+            
+          if __name__=="__main__": 
+              main()             # call main function 
 
-
+except FileNotFoundError:
+  print("text.txt file was not found")
 
 ########################### Useful sources for the script ###########################
 # https://www.geeksforgeeks.org/find-frequency-of-each-word-in-a-string-in-python/
